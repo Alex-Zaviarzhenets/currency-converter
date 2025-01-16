eee63696fb69792b65fe9ebbd8e2539266a99ebf
aliaksei <alex.zaviarzhenets12@gmail.com>
2025-01-16 20:27:43 +0100

    changed buttons to selectors

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const [form] = document.forms
 const [box, input] = document.getElementsByTagName('input')
 const button = document.getElementsByTagName('button')[2]
 const output = document.getElementById('result')
+const select1 = document.getElementById('select1')
+const select2 = document.getElementById('select2')
 let kurs
 
 fetch('/getKurs').then(response => response.text()).then(text => kurs = +text)
 
 update()
 
-form.onclick = update
+select1.addEventListener('change', update);
+select2.addEventListener('change', update);
 
 input.onfocus = removeSign
 
@@ -17,8 +20,19 @@ function update() {
   input.placeholder = !box.checked ? "€" : "$";
   output.value = box.checked ? "€" : "$";
   removeSign()
+  const fromCurrency = select1.value
+  const toCurrency = select2.value
+
+  let convert
+  if(fromCurrency === 'eur' && toCurrency === 'usd') {
+    convert = convertUsdEur
+  } else if (fromCurrency === 'usd' && toCurrency === 'eur') {
+    convert = convertEurUsd
+  } else if (fromCurrency === toCurrency) {
+    input.placeholder = toCurrency === 'eur' ? '€' : '$'
+    output.value = input.placeholder
+  }
   if (input.value) {
-    const convert = box.checked ? convertUsdEur : convertEurUsd
     output.value += convert(input.value, kurs).toFixed(2)
     if (!input.matches(":focus")) {
       input.value = input.placeholder + input.value
